96cba905310c9b8df3e56164938396ee07609479
Ivan Ternovyi <ivanternovyi@gmail.com>
2020-08-15 12:57:32 +0300

    add story model + validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_15_094345) do
+ActiveRecord::Schema.define(version: 2020_08_15_095625) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 2020_08_15_094345) do
     t.string "name"
     t.text "text"
     t.string "article_type"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "stories", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
